1668f816038cbd6cb20988a72b6cc65710cc651c
Matt Pityo <mpityo@gmail.com>
2022-01-16 16:26:38 -0500

    work on quiz questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,9 +5,9 @@ var masterQuestionList = [
         correct: "alerts"
     },
 	{
-        question: "Commonly used data types do NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        question: "Which of the following is NOT a loop function?",
+        answers: ["do-while", "while", "for", "cycle"],
+        correct: "cycle"
     },
 	{
         question: "Commonly used data types do NOT include:",
