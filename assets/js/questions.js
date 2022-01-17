aea04e0ed8773bc333f305f6be40ea9aa07d6024
Matt Pityo <mpityo@gmail.com>
2022-01-17 11:40:09 -0500

    Finish questions and answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,32 +6,32 @@ var masterQuestionList = [
     },
 	{
         question: "Which of the following is NOT a loop function?",
-        answers: ["do-while", "while", "for", "cycle"],
+        answers: ["cycle", "while", "for", "do-while"],
         correct: "cycle"
     },
 	{
-        question: "Commonly used data types do NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        question: "What HTML tag is used to import a JavaScript file?",
+        answers: ["<script>", "<href>", "<p>", "<import>"],
+        correct: "<script>"
     },
 	{
-        question: "Commonly used data types do NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        question: "Which is NOT a CSS display property?",
+        answers: ["relative", "block", "inline", "flex"],
+        correct: "relative"
     },
 	{
-        question: "Commonly used data types do NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        question: "What is the term that describes how a JavaScript file gets read in regards to variables and functions?",
+        answers: ["hoist", "bubble", "bottom-to-top", "query"],
+        correct: "hoist"
     },
 	{
-        question: "Commonly used data types do NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        question: "Which of the following is NOT an operator you can use?",
+        answers: ["@=", "||", "&&", "%="],
+        correct: "@="
     },
 	{
-        question: "Commonly used data types do NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        question: "Which is NOT a way to define a new variable?",
+        answers: ["new", "var", "const", "let"],
+        correct: "new"
     }
 ];
