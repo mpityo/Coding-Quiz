2255a58666fe74d791e65c610564f2a71b97a2c2
Matt Pityo <mpityo@gmail.com>
2022-01-17 16:10:25 -0500

    able to start quiz and start timer, as well as ending quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,45 @@
 console.log(masterQuestionList);
 
-// get button listener to start quiz
-// start timer for 75 seconds
-//  - add timer function
-//  - change content from HTML timer to "Time: XX"
-// remove main content element on screen
+var timerEl = document.querySelector("#timer");
+var startQuizBtn = document.querySelector("#start-quiz");
+
+var mainQuestionAreaEl = document.querySelector(".main-content-area");
+
+var timeToFinish = 10;
+
 // create html content for:
 //  - question (questionHeaderEl)
 //  - answers buttons (answerListEl, answer1Btn - answer4BtnEl)
 //    - add 'btn' as class for CSS themeing
+
+var startQuiz = function () {
+    // create timer, updates on main header bar
+    var timer = setInterval(function() {
+        timerEl.textContent = "Time: " + timeToFinish;
+        if (timeToFinish === 0) {
+            endQuiz();
+        }
+        if (timeToFinish <= 0) {
+            clearInterval(timer);
+        }
+        timeToFinish--;
+    }, 1000);
+
+    // remove button and paragraph from screen
+    startQuizBtn.remove();
+    mainQuestionAreaEl.querySelector("p").remove();
+
+    loadQuestion();
+}
+
+var createQuestionBtns = function () {
+
+}
+
+var loadQuestion = function() {
+    var questionHeaderEl = document.querySelector(".question-spot");
+    questionHeaderEl.textContent = masterQuestionList[0].question;
+}
 
 // load the first (and subsequent) questions by:
 //  - randomize the question array and get a random array slot
@@ -26,7 +57,7 @@ console.log(masterQuestionList);
 //  - display the next question
 // if answer is wrong:
 //  - display "wrong" under question
-//  - subtract 15 seconds from quiz
+//  - subtract 10 seconds from quiz
 //    - (OPTIONAL) - CSS to show removal of time from timer
 //  - display the next question
 
@@ -40,7 +71,9 @@ console.log(masterQuestionList);
 //    - give it class of 'btn' for CSS themeing
 //    - text of "Submit"
 //  - add action listener for submitHighScoreBtn
-
+var endQuiz = function () {
+    alert("Time has ended!");
+}
 //  once submit button has been pressed:
 //  - remove content from main screen
 //  - change questionHeaderEl.content to "High Scores"
@@ -61,3 +94,5 @@ console.log(masterQuestionList);
 //      - clear local storage and delete all list items on screen
 //      - alert() user that scores have been cleared
 //      - reload to main screen
+
+startQuizBtn.addEventListener("click", startQuiz);
